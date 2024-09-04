126ff2c2f373a46b1d894b8893f5456bd86e2a0f
xuyangbocn <xuyangbocn@users.noreply.github.com>
2024-09-04 21:02:29 +0800

    handle present directory with space or special char

--- create_layer.sh
+++ create_layer.sh
@@ -11,8 +11,8 @@ PY_VERSION="3.12"
 DOCKER_IMAGE="python:${PY_VERSION}"
 
 # pip install in docker container
-docker run --platform ${PLATFORM} -v $(pwd):${MOUNT_PATH} ${DOCKER_IMAGE} \
+docker run --platform ${PLATFORM} -v "$(pwd):${MOUNT_PATH}" ${DOCKER_IMAGE} \
 pip3 install -r ${MOUNT_PATH}/requirements.txt -t ${MOUNT_PATH}/${PKG_PATH}
 
 # zip it
-zip -r $(pwd)/layer_to_upload.zip ${PKG_PATH}
+zip -r "$(pwd)/layer_to_upload.zip" ${PKG_PATH}
